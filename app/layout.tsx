575238d54b3138c4aaafc0408760a474d694f27e
Pritish <pritish@theclamsleep.com>
2023-06-08 20:44:15 +0530

    grid background

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "ThreadShift",
-  description: "Generated Linkedin Carousels from Twitter Threads in seconds.",
+  description: "Generated Linkedin Carousels from Twitter Threads seamlessly.",
 };
 
 export default function RootLayout({
@@ -17,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col justify-center bg-white text-black">
+          <div className="absolute z-0 h-full w-full bg-[url('/grid.svg')] opacity-10 [background-position:calc(100%+1px)_calc(100%+30px)]"></div>
           {children}
         </div>
       </body>
